1478d2f6c27f3f814feb08972d512ded00b01747
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2024-10-09 20:03:14 -0300

    se realiza modificación en el login, funciona y redirecciona segun el rol

--- vite-project/src/components/FormularioIniciarSesion.jsx
+++ vite-project/src/components/FormularioIniciarSesion.jsx
@@ -5,64 +5,65 @@ import React, { useEffect, useState } from "react";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 
-const FormularioIniciarSesion = ({setUsuarioLogueado}) => {
+const FormularioIniciarSesion = ({ setUsuarioLogueado }) => {
   const navigate = useNavigate();
 
   const [formInputs, setFormInputs] = useState({
-    usuario: '',
-    password: ''
+    email: "",
+    contrasenia: "",
   });
 
-  const [usuarioInput, setusuarioInput] = useState(false);
-  const [passwordInput, setpasswordInput] = useState(false);
+  const [usuarioInput, setUsuarioInput] = useState(false);
+  const [contraseniaInput, setContraseniaInput] = useState(false);
 
   const handleChange = (ev) => {
     setFormInputs({ ...formInputs, [ev.target.name]: ev.target.value });
-    
-    if (ev.target.name === 'usuario' && ev.target.value) {
-      setusuarioInput(false);
-    } else if (ev.target.name === 'password' && ev.target.value) {
-      setpasswordInput(false);
+
+    if (ev.target.name === "email" && ev.target.value) {
+      setUsuarioInput(false);
+    } else if (ev.target.name === "contrasenia" && ev.target.value) {
+      setContraseniaInput(false);
     }
   };
 
   const handleClick = async (ev) => {
     ev.preventDefault();
-    if (formInputs.usuario) {
-      if (formInputs.password) {
-        setusuarioInput(false);
-        setpasswordInput(false);
-
-        try {      
-           const res = await fetch('http://localhost:4000/apiStock/', {
-            method: 'POST',
-            headers: {
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-              usuario: formInputs.usuario, 
-              password: formInputs.password
-            })
-          });
-
+    if (formInputs.email) {
+      if (formInputs.contrasenia) {
+        setUsuarioInput(false);
+        setContraseniaInput(false);
+        try {
+          const res = await fetch(
+            "http://localhost:4000/apiStock/iniciarSesion",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                email: formInputs.email,
+                contrasenia: formInputs.contrasenia,
+              }),
+            }
+          );
           const data = await res.json();
+          console.log(data);
           if (data.usuarioExist) {
-            localStorage.setItem('token', data.usuarioExist.token);
-            localStorage.setItem('role', data.usuarioExist.role);
-            localStorage.setItem('nombres', data.usuarioExist.nombres);
-            console.log(data.usuarioExist)
-             if (data.usuarioExist.role === 'admin') {
-              navigate('/Inicioadmin');
-            } else if (data.usuarioExist.role === 'usuario') {
-              navigate('/');
-            } 
+            localStorage.setItem("token", data.usuarioExist.token);
+            localStorage.setItem("rol", data.usuarioExist.rol);
+            localStorage.setItem("nombres", data.usuarioExist.nombres);
+            console.log("Usuario logueado:", data.usuarioExist);
+            if (data.usuarioExist.rol === "admin") {
+              navigate("/InicioAdmin");
+            } else if (data.usuarioExist.rol === "user") {
+              navigate("/");
+            }
           } else {
-            console.error("Usuario no encontrado o credenciales incorrectas");
             Swal.fire({
               position: "center",
               icon: "error",
               title: "Error en la solicitud de login",
-              text: "Usuario no encontrado o credenciales incorrectas" ,
+              text: "Usuario no encontrado o credenciales incorrectas",
               showConfirmButton: false,
               timer: 1380,
             });
@@ -73,17 +74,17 @@ const FormularioIniciarSesion = ({setUsuarioLogueado}) => {
             position: "center",
             icon: "error",
             title: "Error",
-            text: "Error en la solicitud de login: ", error ,
+            text: "Ocurrió un error al iniciar sesión",
             showConfirmButton: false,
             timer: 1380,
           });
         }
       } else {
-        setpasswordInput(true);
+        setContraseniaInput(true);
       }
     } else {
-      setusuarioInput(true);
-      setpasswordInput(true);
+      setUsuarioInput(true);
+      setContraseniaInput(true);
     }
   };
 
@@ -93,36 +94,44 @@ const FormularioIniciarSesion = ({setUsuarioLogueado}) => {
 
   return (
     <Container fluid style={{ background: "#E1F7F5" }}>
-    <Row>
-      <Col className="d-flex justify-content-center">
-        <Form className="p-5 w-50 text-center">
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Ingresar correo electrónico</Form.Label>
-            <Form.Control
-              name="usuario"
-              onChange={handleChange}
-              className={usuarioInput ? ' form-control is-invalid' : 'form-control'}
-              type="email"
-              placeholder=""
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicpasswordword">
-            <Form.Label>Ingresar contraseña</Form.Label>
-            <Form.Control
-              name="password"
-              onChange={handleChange}
-              className={passwordInput ? 'form-control is-invalid' : 'form-control'}
-              type="password"
-              placeholder=""
-            />
-          </Form.Group>
-          <Button  style={{background:"#0E46A3", color:"#E1F7F5"}}  onClick={handleClick} type="submit">
-            Ingresar
-          </Button>
-        </Form>
-      </Col>
-    </Row>
-  </Container>
+      <Row>
+        <Col className="d-flex justify-content-center">
+          <Form className="p-5 w-50 text-center">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Ingresar correo electrónico</Form.Label>
+              <Form.Control
+                name="email"
+                onChange={handleChange}
+                className={
+                  usuarioInput ? "form-control is-invalid" : "form-control"
+                }
+                type="email"
+                placeholder=""
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasiccontraseniaword">
+              <Form.Label>Ingresar contraseña</Form.Label>
+              <Form.Control
+                name="contrasenia"
+                onChange={handleChange}
+                className={
+                  contraseniaInput ? "form-control is-invalid" : "form-control"
+                }
+                type="password"
+                placeholder=""
+              />
+            </Form.Group>
+            <Button
+              style={{ background: "#0E46A3", color: "#E1F7F5" }}
+              onClick={handleClick}
+              type="submit"
+            >
+              Ingresar
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
